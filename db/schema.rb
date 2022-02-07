521531e2dc4be69af8cbe48ccde7f17c3f0b2b59
enmanuel <enmanuellassis@gmail.com>
2022-02-07 18:42:04 -0400

    add sign in and sign up css

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_02_07_190154) do
+ActiveRecord::Schema.define(version: 2022_02_07_195013) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -34,13 +34,13 @@ ActiveRecord::Schema.define(version: 2022_02_07_190154) do
     t.index ["user_id"], name: "index_groups_on_user_id"
   end
 
-  create_table "groups_expenses", id: false, force: :cascade do |t|
-    t.bigint "group_id"
-    t.bigint "expense_id"
+  create_table "relations", id: false, force: :cascade do |t|
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.index ["expense_id"], name: "index_groups_expenses_on_expense_id"
-    t.index ["group_id"], name: "index_groups_expenses_on_group_id"
+    t.bigint "group_id", null: false
+    t.bigint "expense_id", null: false
+    t.index ["expense_id"], name: "index_relations_on_expense_id"
+    t.index ["group_id"], name: "index_relations_on_group_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -58,4 +58,6 @@ ActiveRecord::Schema.define(version: 2022_02_07_190154) do
 
   add_foreign_key "expenses", "users"
   add_foreign_key "groups", "users"
+  add_foreign_key "relations", "expenses"
+  add_foreign_key "relations", "groups"
 end
---
+++ spec/models/relation_spec.rb
@@ -0,0 +1,14 @@
+require 'rails_helper'
+
+RSpec.describe Relation, type: :model do
+  subject { Relation.new(group_id:1,expense_id:1) }
+  before { subject.save }
+  it 'should be invalid if group_id is nil' do
+    subject.group_id = nil
+    expect(subject).to_not be_valid
+  end
+  it 'should be invalid if expense_id is nil' do
+    subject.expense_id = nil
+    expect(subject).to_not be_valid
+  end
+end
